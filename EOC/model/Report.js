00e9d20c07c8182b3df9efb2bf1cbfa7e772d428
gunashekar <koppulagunashekar1994@gmail.com>
2019-11-18 08:13:11 -0600

    changed report model

---
+++ EOC/model/Report.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+autoIncrement = require('mongoose-auto-increment');
+// Create Schema
+const ReportSchema = new Schema({
+  reportId:{
+    type: Number, 
+    default: 0, 
+    unique: true
+  },
+  incidentId: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  dateAndTime: {
+    type: Date,
+    required: true
+  },
+  typeOfIncident: {
+    type: String,
+  },
+  levelOfImpact: {
+    type: String,
+  },
+  hazmatType: {
+    type: String,
+  },
+  notes: {
+    type: String,
+  },
+  Casualities:{
+        Red: { type: Number },
+        Yellow: { type: Number },
+        Green: { type: Number },
+        Black: { type: Number }     
+  }
+  });
+autoIncrement.initialize(mongoose.connection);
+ReportSchema.plugin(autoIncrement.plugin, {
+  model: 'reports',
+  field: 'reportId',
+  startAt: 1,
+  incrementBy: 1
+});
+
+module.exports = Incidents = mongoose.model("reports", ReportSchema);
